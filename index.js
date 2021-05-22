f0a89fce687c517b114bddef52323efe051ac0b5
Sergio Ruix <srf28@alu.ua.es>
2021-05-23 00:56:37 +0200

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
-// const request = require('request');
+const request = require('request');
 
 var achievements1, achievements2, total
 
@@ -16,17 +16,17 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 router.get("/", (req, res) => {
-    // request.get(url1, (error, res, body) => {
-    //     achievements1 = JSON.parse(body)
-    //     achievements1 = getAchievements(achievements1)
-    // });
-    // request.get(url2, (error, res, body) => {
-    //     achievements2 = JSON.parse(body)
-    //     achievements2 = getAchievements(achievements2)
-    // });
-    // console.log(achievements2);
-    // total = achievements2 - achievements1
-    // res.render("index", {total: total});
+    request.get(url1, (error, res, body) => {
+        achievements1 = JSON.parse(body)
+        achievements1 = getAchievements(achievements1)
+    });
+    request.get(url2, (error, res, body) => {
+        achievements2 = JSON.parse(body)
+        achievements2 = getAchievements(achievements2)
+    });
+    console.log(achievements2);
+    total = achievements2 - achievements1
+    res.render("index", {total: total});
     res.render("index");
 });
 
@@ -35,6 +35,6 @@ app.listen(process.env.PORT, '0.0.0.0');
 
 console.log("Running at Port 000");
 
-// function getAchievements(achievement) {
-//     return count = Object.entries(achievement.playerstats.achievements).length;
-// }
+function getAchievements(achievement) {
+    return count = Object.entries(achievement.playerstats.achievements).length;
+}
