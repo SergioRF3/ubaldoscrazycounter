4cae8547c6879381a6acbd19482b1e3642795fae
Sergio Ruix <srf28@alu.ua.es>
2021-05-23 01:40:02 +0200

    v01

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var url1='http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/' +
 'v0002/?appid=250900&key=424CF0DAB58DA8B0F8A8B9A037AA93E5&steamid=76561198079199657';
 
 var url2='http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/' +
-'v0002/?appid=250900&key=424CF0DAB58DA8B0F8A8B9A037AA93E5&steamid=76561198079199657';
+'v0002/?appid=250900&key=424CF0DAB58DA8B0F8A8B9A037AA93E5&steamid=76561198114457291';
 
 const express = require("express");
 const app = express();
@@ -33,7 +33,7 @@ router.get("/", (req, res) => {
 app.use("/", router);
 app.listen(process.env.PORT, '0.0.0.0');
 
-console.log("Running at Port 000");
+console.log("Running at Port 0000");
 
 function getAchievements(achievement) {
     return count = Object.entries(achievement.playerstats.achievements).length;
