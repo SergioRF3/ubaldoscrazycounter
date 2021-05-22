91db0853af001d34292b7479071c57db50cd761c
Sergio Ruix <srf28@alu.ua.es>
2021-05-23 00:22:41 +0200

    test

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ router.get("/", (req, res) => {
 });
 
 app.use("/", router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 5000);
 
 console.log("Running at Port 3000");
 
